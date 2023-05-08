d7173fae130c4337f1c37ee39c4632521cd1525f
lyocikk <lyocikk@gmail.com>
2023-05-08 23:01:26 +0800

    modified some styles

---
+++ app/static/style.js
@@ -0,0 +1,8 @@
+// base.html
+const currentUrl = window.location.href;
+const links = document.querySelectorAll('.navbar a');
+links.forEach((link) => {
+    if (link.href === currentUrl) {
+        link.classList.add('active');
+    }
+});
