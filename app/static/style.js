8d909118b6e74f7ae7b889e07d178917c30f5b82
lyocikk <lyocikk@gmail.com>
2023-05-16 16:31:22 +0800

    edited nav bars

--- app/static/style.js
+++ app/static/style.js
@@ -8,21 +8,6 @@ links.forEach((link) => {
 });
 
 
-// function dropdown() {
-//     document.getElementById("dropdown-menu").classList.toggle("show");
-// }
-// window.onclick = function(event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (var i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
 function dropdown() {
     var dropdownMenu = document.getElementById("dropdown-menu");
     if (dropdownMenu.style.display === "block") {
@@ -31,4 +16,5 @@ function dropdown() {
         dropdownMenu.style.display = "block";
     }
 }
+
 
