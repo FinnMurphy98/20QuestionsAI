038dada68b7d525fef4490d7e98d303ac1905544
lyocikk <lyocikk@gmail.com>
2023-05-16 15:44:37 +0800

    edited styles of nav bar

--- app/static/style.js
+++ app/static/style.js
@@ -8,17 +8,27 @@ links.forEach((link) => {
 });
 
 
+// function dropdown() {
+//     document.getElementById("dropdown-menu").classList.toggle("show");
+// }
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (var i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 function dropdown() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
     }
 }
+
