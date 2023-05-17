51d91be1b86548ed77f34752290beece126104bc
lyocikk <lyocikk@gmail.com>
2023-05-18 01:23:54 +0800

    edit some code for socket

--- app/static/style.js
+++ app/static/style.js
@@ -18,26 +18,5 @@ function dropdown() {
 }
 
 
-// history
-// function of changing page
-function changePage(direction) {
-    var currentPage = parseInt(document.getElementById("current-page").textContent);
-    var totalPages = parseInt(document.getElementById("total-pages").textContent);
-    var jumpInput = document.getElementById("jump-input");
-    
-    if (direction === "prev" && currentPage > 1) {
-        window.location.href = "{{ url_for('app.history') }}?page=" + (currentPage - 1);
-    } else if (direction === "next" && currentPage < totalPages) {
-        window.location.href = "{{ url_for('app.history') }}?page=" + (currentPage + 1);
-    }
-}
 
-// function of jumping to page by input 
-function jumpToPage() {
-    var jumpInput = document.getElementById("jump-input");
-    var page = parseInt(jumpInput.value);
-    
-    if (!isNaN(page) && page >= 1 && page <= {{ games.pages }}) {
-            window.location.href = "{{ url_for('app.history') }}?page=" + page;
-    }
-}
+
